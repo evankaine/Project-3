f6a4ad03e9fcbed242da660ddcdf89f149135514
Henry Patterson <henrycpatterson92@gmail.com>
2021-08-03 15:39:28 -0400

    layout

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from "./Navbar"
-import Footer from "../Footer/Footer"
+
 
 export default function Layout(props) {
   return (
     <div>
       <Navbar user={props.user} setUser={props.setUser} />
       <div>{props.children}</div>
-      <Footer/>
+      
     </div>
   )
 }
