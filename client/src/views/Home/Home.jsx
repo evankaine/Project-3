ac332780e742932829aa0b17d09d3890c8680848
Christopher Sharif <chrissharif@Christophers-MacBook-Air.local>
2021-08-03 11:41:16 -0400

    Started Home Component

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export default function Home() {
   return (
     <div>
-      
+      <nav>
+        <header>imgNation</header>
+        <form>
+          <input className='search-bar' placeholder='Search...'></input>
+        </form>
+        <div>
+          <AddAPhotoIcon className='add-photo-icon' />
+          <AccountCircleIcon className='account-icon' />
+        </div>
+      </nav>
+      {/* Posts here */}
     </div>
   )
 }
