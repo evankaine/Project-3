7167e9f08664a1e0037ee4f7e4db16b20fc145c1
diego <dgvlc@yahoo.com>
2021-08-03 07:31:59 -0400

    adding initial frontend stuff

---
+++ client/src/services/posts.js
@@ -0,0 +1,20 @@
+import api from "./apiConfig"
+
+//rename
+export const getAllPost = async () => {
+  try {
+    const res = await api.get("/posts")
+    return res.data
+  } catch (e) {
+    throw e
+  }
+}
+//rename
+export const createPost = async (input) => {
+  try {
+    const res = await api.post("/posts", input);
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+};
