f5ddbd1e5ccb1226b6ac9d3a2b27810e5367c107
Evan Alves <evanalves@Evans-MacBook-Air.local>
2021-08-03 15:35:22 -0400

    some styling for sign in page

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import SignUp from "../components/SignUp"
+import React from 'react';
+import './login.css'
+import SignUp from "../components/SignUp";
+import SignIn from "../components/SignIn";
+
 
 export default function Login() {
   return (
-    <div>
-      <SignUp />
+    
+    <div className="wrapper">
+      <div className="form">
+        <div className="title">
+          <h1>imgNation</h1>
+        </div>
+        <SignIn />
+        <br />
+        <SignUp />
+      </div>
     </div>
+      
   )
 }
