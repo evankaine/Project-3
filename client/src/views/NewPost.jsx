683d378a4485f52186eb50157af95dccb1d8a850
Henry Patterson <henrycpatterson92@gmail.com>
2021-08-04 15:32:38 -0400

    newpost

--- client/src/views/NewPost.jsx
+++ client/src/views/NewPost.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./newpost.css"
-import {createPost} from "../services/apiConfig"
+import { createPost } from "../services/apiConfig"
+import NavBar from "../components/navbar/Navbar"
 import { useHistory } from "react-router";
 
 export default function NewPost(props) {
@@ -23,8 +24,6 @@ export default function NewPost(props) {
   };
   
   return (
-
-    <div className="wrapper" user={props.user} setUser={props.setUser}>
       <div className="form">
         <h1 className="title">Make a Post</h1>
         <form onSubmit={handleSubmit}>
