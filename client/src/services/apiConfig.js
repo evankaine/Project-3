f22e32dcbfadf8b24850348c3b9e650a610427df
diego <dgvlc@yahoo.com>
2021-08-05 11:36:09 -0400

    acquiring auth

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -29,8 +29,8 @@ export const getPost = async (id) => {
     return res.data
 }
 //create a post
-export const createPost = async (input) => {
-    const res = await axios.post(`${apiURL}/posts`, input)
+export const createPost = async (post) => {
+    const res = await axios.post(`${apiURL}/posts`, post)
     return res.data
 }
 //delete a post
